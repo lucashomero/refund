83a97213dfcd387d4d6ff1db617949a5d12347f9
lucashomero <lucashomerovs@gmail.com>
2025-04-10 09:03:21 -0300

    feat: create object with new expense details

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Seleciona os elementos do formulario
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Obtem o valor do input, aceitando somente numeros
 amount.oninput = () => {
@@ -17,4 +20,19 @@ function formatCurrencyBRL(value) {
         currency: "BRL",
     })
     return value
+}
+
+// Captura o evento de submit do formulario
+form.onsubmit = (event) => {
+    event.preventDefault()
+    // Cria um objeto com os detalhes da nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+      }
+      console.log(newExpense)
 }
